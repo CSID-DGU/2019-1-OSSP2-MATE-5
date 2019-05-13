0bea72f9ce3f25358613d9834e2d1132aaae9cd8
ShinSeongHyeon <48276553+ShinSeongHyeon@users.noreply.github.com>
2019-05-13 14:12:57 +0900

    Update MainActivity.java
    변수명 수정

--- Android-OCRSample-master/app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
+++ Android-OCRSample-master/app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
@@ -180,19 +180,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-             //핸드폰 번호만 추출 . - 다 가능
+            //핸드폰 번호만 추출 . - 다 가능
             String inputString = detectedText.toString();
             String result = inputString.replaceAll("01(?:0|1|[6-9]).(\\d{4}).(\\d{4})","");
-            String phoneNumber = inputString.replace(result,"");
+            String data = inputString.replace(result,"");
 
             //텍스트뷰에보여줌
-            detectedTextView.setText(phoneNumber);
+            detectedTextView.setText(data);
 
             //전송전에 미리 .이나 - 삭제
-            if(phoneNumber.contains("-"))
-                phoneNumber=phoneNumber.replace("-","");
-            else if(phoneNumber.contains("."))
-                phoneNumber=phoneNumber.replace(".","");
+            if(data.contains("-"))
+                data=data.replace("-","");
+            else if(data.contains("."))
+                data=data.replace(".","");
 
 
         }
