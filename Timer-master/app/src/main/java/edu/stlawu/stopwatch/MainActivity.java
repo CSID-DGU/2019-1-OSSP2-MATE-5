e89ff9fe8ac098c68c5bdd2a7ada7bb6d334664a
Kyung <roodwl@naver.com>
2019-06-15 04:58:50 +0900

    combine login, register, autocall todo : random number

--- Timer-master/app/src/main/java/edu/stlawu/stopwatch/MainActivity.java
+++ Timer-master/app/src/main/java/edu/stlawu/stopwatch/MainActivity.java
@@ -1,21 +1,24 @@
 package edu.stlawu.stopwatch;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.Manifest;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.RemoteException;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +26,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.internal.telephony.ITelephony;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,14 +47,9 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.android.internal.telephony.ITelephony;
-
 public class MainActivity extends AppCompatActivity {
 
     protected static final String TAG = "MainActivity";
-
-    private int defCallInterval = 10 * 1000;
-    private int defHangUpInterval = 10 * 1000;
 
     private static final int startCall = 1001;
     private static final int stopCall = 1002;
@@ -59,39 +63,28 @@ public class MainActivity extends AppCompatActivity {
     private Button bt_reset = null;
     private Timer t = null;
     private Counter ctr = null; //Timertask
+    private AlertDialog dialog;
+
+    String userID = LoginActivity.userID;
 
     // audio variables
     private AudioAttributes aa = null;
     private SoundPool soundPool = null;
     private int bloopSound = 0;
 
-
-    /**
-     * Called when the activity is first created.
-     */
-    private Button btn_start = null;
-    private Button btn_stop = null;
-    private EditText et_phone_no = null;
-    private EditText et_call_interval = null;
-    private EditText et_hand_up_interval = null;
-    private boolean isRunnable = true;
-    private boolean endCall = false;
-    private String telePhotoNo = null;
-    private String callInterval = null;
-    private String hangUpInterval = null;
-    ITelephony iPhoney = null;
-
     String url = "http://hssoft.kr:9878/phone.php";
     public GettingPHP gPHP;
 
-    boolean isFirstStart = true;
-
-    // 检查权限
+    ITelephony iPhoney = null;
     PackageManager pm = null;
+    TelephonyManager tm = null;
+
+    boolean isFirstStart = true;
     boolean callPhonePermission = false;
     boolean readPhoneStatePermission = false;
+    private boolean isRunnable = true;
+    private boolean endCall = false;
 
-    TelephonyManager tm = null;
 
     private Handler mHandler = new Handler() {
         public void handleMessage(Message msg) {
@@ -109,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
             super.handleMessage(msg);
         }
     };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -120,10 +115,11 @@ public class MainActivity extends AppCompatActivity {
         this.bt_stop = findViewById(R.id.bt_stop);
         this.bt_reset = findViewById(R.id.bt_reset);
 
-        et_call_interval = (EditText) findViewById(R.id.et_call_interval);
-        et_hand_up_interval = (EditText) findViewById(R.id.et_hand_up_interval);
+        bt_stop.setEnabled(false);
+        bt_reset.setEnabled(false);
 
-        no=findViewById(R.id.no);
+        no = findViewById(R.id.no);
+
         pm = getPackageManager();
         callPhonePermission = (PackageManager.PERMISSION_GRANTED ==
                 pm.checkPermission("android.permission.CALL_PHONE", this.getPackageName()));
@@ -139,24 +135,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Check if the permission already exits
-                int permission= ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CALL_PHONE);
-                if(permission== PackageManager.PERMISSION_GRANTED)
-                {
+                int permission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+                if (permission == PackageManager.PERMISSION_GRANTED) {
                     gPHP = new GettingPHP();
-                    no = (TextView)findViewById(R.id.no);
-                    gPHP.execute(url);
-                    telePhotoNo=no.getText().toString();
-
+                    no = (TextView) findViewById(R.id.no);
                     isRunnable = true;
+                    gPHP.execute(url);
                     startCallPhone();
-                }
-                else
-                {
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},121);
+                    //  callNumber();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 121);
                 }
                 bt_start.setEnabled(false);
                 bt_stop.setEnabled(true);
-                bt_reset.setEnabled(true);
+                bt_reset.setEnabled(false);
                 resume();
             }
         });
@@ -171,25 +163,74 @@ public class MainActivity extends AppCompatActivity {
                 bt_reset.setEnabled(true);
                 getPreferences(MODE_PRIVATE).edit().putInt("COUNT", ctr.count).apply();
 
-
                 isRunnable = false;
                 ctr.cancel();
             }
         });
 
 
-        // reset button
+        // SEND button
         this.bt_reset.setOnClickListener(new View.OnClickListener() {
                                              @Override
                                              public void onClick(View v) {
+                                                 int hour = ctr.count / 36000;
+                                                 String hour_s = String.format("%02d", hour);
+                                                 int min = (ctr.count / 600) % 60;
+                                                 String min_s = String.format("%02d", min);
+                                                 int sec = (ctr.count / 10) % 60;
+                                                 String sec_s = String.format("%02d", sec);
+
+                                                 String userTime = hour_s + min_s + sec_s;
+
+                                                 Response.Listener<String> responseListen = new Response.Listener<String>() {
+
+                                                     @Override
+                                                     public void onResponse(String response) {
+                                                         try {
+                                                             JSONObject jsonResponse = new JSONObject(response);
+                                                             //boolean success = jsonResponse.getBoolean("success");
+                                                             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                                             dialog = builder.setMessage("SEND Success")
+                                                                     .setPositiveButton("확인", null)
+                                                                     .create();
+                                                             dialog.show();
+//                             if(success){
+//                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                                 dialog = builder.setMessage("SEND Success")
+//                                         .setPositiveButton("확인", null)
+//                                         .create();
+//                                 dialog.show();
+////                                 // reset count
+////                                 getPreferences(MODE_PRIVATE).edit().putInt("COUNT", 0).apply();
+////                                 ctr.cancel();
+////                                 // set text view back to zero
+////                                 MainActivity.this.tv_count.setText("00:00:00:00");
+//                             }else {
+//                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                                 dialog = builder.setMessage("SEND failed")
+//                                         .setNegativeButton("확인", null)
+//                                         .create();
+//                                 dialog.show();
+//                             }
+
+                                                         } catch (Exception e) {
+                                                             e.printStackTrace();
+                                                         }
+                                                     }
+                                                 };
+
+                                                 TimeRequest timeRequest = new TimeRequest(userID, userTime, responseListen);
+                                                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                                                 queue.add(timeRequest);
+
                                                  bt_start.setEnabled(true);
                                                  bt_start.setText("Start");
                                                  bt_stop.setEnabled(false);
                                                  // reset count
-                                                 //getPreferences(MODE_PRIVATE).edit().putInt("COUNT", 0).apply();
+                                                 getPreferences(MODE_PRIVATE).edit().putInt("COUNT", 0).apply();
                                                  ctr.cancel();
                                                  // set text view back to zero
-                                                 //MainActivity.this.tv_count.setText("00:00.0");
+                                                 MainActivity.this.tv_count.setText("00:00:00:00");
                                              }
                                          }
 
@@ -198,37 +239,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    void startCallPhone () {
-        if(callPhonePermission) {
-            if(isFirstStart){
-                isFirstStart = false;
-            }
-            else {
-                try{
-                    Thread.sleep(5000);
 
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-            if(isRunnable) {
-                int state = -1;
-
-                String telno = no.getText().toString();
-                Uri uri = Uri.parse("tel:" + telno);
-                Intent i = new Intent(Intent.ACTION_CALL, uri);
-                startActivity(i);
-                Log.d(TAG, "Calling---------------");
-
-                Message msg = Message.obtain();
-                msg.what = stopCall;
-                mHandler.sendMessage(msg);
-            }
-            else
-                Log.d(TAG, "isRunnable false-----------");
+    void callNumber() {
+        String telno = no.getText().toString();
+        Uri uri = Uri.parse("tel:" + telno);
+        Intent i = new Intent(Intent.ACTION_CALL, uri);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
-        else
-            Log.d(TAG, "callpermission failed---------");
+        startActivity(i);
     }
 
 
@@ -240,7 +266,7 @@ public class MainActivity extends AppCompatActivity {
         // getint needs a default value
         int count = getPreferences(MODE_PRIVATE).getInt("COUNT", 0);
 
-        this.tv_count.setText(String.format("%02d:%02d:%02d:%02d", count/3600, count / 600, (count / 10) % 60, count % 10));
+        this.tv_count.setText(String.format("%02d:%02d:%02d:%02d", count / 3600, count / 600, (count / 10) % 60, count % 10));
         System.out.println(count);
 
         // create timer
@@ -259,8 +285,8 @@ public class MainActivity extends AppCompatActivity {
         t.scheduleAtFixedRate(ctr, 0, 100);
     }
 
-    public void set_display(){
-        this.tv_count.setText(String.format("%02d:%02d:%02d:%02d", ctr.count/3600, ctr.count / 600, (ctr.count / 10) % 60, ctr.count % 10));
+    public void set_display() {
+        this.tv_count.setText(String.format("%02d:%02d:%02d:%02d", ctr.count / 3600, ctr.count / 600, (ctr.count / 10) % 60, ctr.count % 10));
 
     }
 
@@ -282,7 +308,7 @@ public class MainActivity extends AppCompatActivity {
         getPreferences(MODE_PRIVATE).edit().putInt("COUNT", ctr.count).apply();
     }
 
-    class Counter extends TimerTask{
+    class Counter extends TimerTask {
         private int count = 0;
 
         @Override
@@ -297,33 +323,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void startCallPhone() {
+        if (callPhonePermission) {
+            if (isFirstStart) {
+                isFirstStart = false;
+            } else {
+                try {
+                    Thread.sleep(5000);
 
-    private static ITelephony getITelephony(Context context) {
-        TelephonyManager mTelephonyManager = (TelephonyManager) context
-                .getSystemService(TELEPHONY_SERVICE);
-        Class<TelephonyManager> c = TelephonyManager.class;
-        Method getITelephonyMethod = null;
-        try {
-            getITelephonyMethod = c.getDeclaredMethod("getITelephony",
-                    (Class[]) null); // 获取声明的方法
-            getITelephonyMethod.setAccessible(true);
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (isRunnable) {
+                int state = -1;
+
+                Log.d(TAG, "TRY Calling---------------");
+                String telno = no.getText().toString();
+                Uri uri = Uri.parse("tel:" + telno);
+                Intent i = new Intent(Intent.ACTION_CALL, uri);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details
+                }
+                startActivity(i);
+                Log.d(TAG, "Calling---------------");
+
+                Message msg = Message.obtain();
+                msg.what = stopCall;
+                mHandler.sendMessage(msg);
+
+            }
+            else
+                Log.d(TAG, "isRunnable false-----------");
         }
-        ITelephony iTelephony = null;
-        try {
-            iTelephony = (ITelephony) getITelephonyMethod.invoke(
-                    mTelephonyManager, (Object[]) null); // 获取实例
-            return iTelephony;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return iTelephony;
+        else
+            Log.d(TAG, "callpermission failed---------");
     }
-
-
     private void stopCallPhone() {
 
         try {
@@ -354,26 +395,33 @@ public class MainActivity extends AppCompatActivity {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-//
-//            if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
-//                do {
-//                    endCall = iPhoney.endCall();
-//                    Log.d(TAG, "挂断电话============== " + endCall);
-//                } while (!endCall);
-//
-//                Message msg = Message.obtain();
-//                msg.what = startCall;
-//                mHandler.sendMessage(msg);
-//            }
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (RemoteException e) {
-//            e.printStackTrace();
-//        }
-//        //System.out.println("是否成功挂断："+endCall);
+
     }
 
+    private static ITelephony getITelephony(Context context) {
+        TelephonyManager mTelephonyManager = (TelephonyManager) context
+                .getSystemService(TELEPHONY_SERVICE);
+        Class<TelephonyManager> c = TelephonyManager.class;
+        Method getITelephonyMethod = null;
+        try {
+            getITelephonyMethod = c.getDeclaredMethod("getITelephony",
+                    (Class[]) null); // 获取声明的方法
+            getITelephonyMethod.setAccessible(true);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        ITelephony iTelephony = null;
+        try {
+            iTelephony = (ITelephony) getITelephonyMethod.invoke(
+                    mTelephonyManager, (Object[]) null); // 获取实例
+            return iTelephony;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return iTelephony;
+    }
     class GettingPHP extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
@@ -412,6 +460,7 @@ public class MainActivity extends AppCompatActivity {
                 Random random = new Random();
                 int i=random.nextInt(result_num);
                 JSONObject temp = results.getJSONObject(i);
+
                 String randomPhoneNumber = temp.get("phonenum").toString();
                 System.out.println(randomPhoneNumber);
                 no.setText(randomPhoneNumber);
@@ -420,7 +469,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
 
 }
