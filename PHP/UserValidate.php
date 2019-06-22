4af6c02a4c007b3f37c8b865f2a11b15091b58af
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2019-06-23 01:39:25 +0900

    Add files via upload

---
+++ PHP/UserValidate.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: application/json");
+	$servername = "localhost";
+    $username = "wndnjs9878";
+    $password = "";
+    $dbname = "phone";
+
+     $con = new mysqli($servername, $username, $password, $dbname);     
+	 $userID = $_POST['userID'];
+     $statement = mysqli_prepare($con, "SELECT userID FROM USER WHERE userID = ?");
+     mysqli_stmt_bind_param($statement, "s", $userID);
+     mysqli_stmt_execute($statement);
+     mysqli_stmt_store_result($statement);
+     mysqli_stmt_bind_result($statement, $userID);
+     $response = array();
+     $response["success"] = true;
+     while(mysqli_stmt_fetch($statement)){
+       $response["success"] = false;//회원가입불가를 나타냄
+       $response["userID"] = $userID;
+     }
+     //데이터베이스 작업이 성공 혹은 실패한것을 알려줌
+     echo json_encode($response);
+?>
---
+++ PHP/phone.php
@@ -0,0 +1,34 @@
+<?php
+	header("Content-Type: application/json");
+
+	$servername = "localhost";
+	$username = "wndnjs9878";
+	$password = "";
+	$dbname = "phone";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	if($conn->connect_error){
+		die("connection failed: ". $conn->connect_error);
+	}
+
+	$sql = "select * from phone";
+	$result = $conn->query($sql);
+	
+	$data = array();
+
+	if($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$data[] = array(
+				"id" => $row["id"],
+				"phonenum" => $row["phonenum"]
+			);
+		}
+		echo json_encode($data);
+	} else {
+		echo "0 results";
+	}
+
+	$conn->close();
+?>
+
